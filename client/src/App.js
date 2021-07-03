647d86e3003276506e017fd7474b16cf3b9ac187
Mert18 <mertuygur02@gmail.com>
2021-07-04 00:01:19 +0300

    hll

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="container">
+      <header className="header">
+        <h2>Diode Haven</h2>
+        <p>Diodes of the world, unite.</p>
+      </header>
+    </div>
+  )
+}
+
+export default App
